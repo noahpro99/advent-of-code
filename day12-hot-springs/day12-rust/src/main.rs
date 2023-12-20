55e63d38e1498cb85a6e74708251c7d80c78da9e
noahpro <noahpro@gmail.com>
2023-12-19 22:28:14 -0500

    remake recursive

--- day12-hot-springs/day12-rust/src/main.rs
+++ day12-hot-springs/day12-rust/src/main.rs
@@ -1,102 +1,38 @@
 use itertools::Itertools;
-use regex::Regex;
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
-fn valid(p: &String, n: Vec<i32>) -> bool {
-    let re = Regex::new(r"\#+").unwrap();
-    let lengths = re
-        .find_iter(p)
-        .map(|m| (m.end() - m.start()) as i32)
-        .collect_vec();
-    n == lengths
-}
-
-
-fn variants(s: &String) -> Vec<String> {
-    // if last char is a ? replace it with either a # or a .
-    let mut v = Vec::new();
-    let mut s = s.clone();
-    if s.ends_with("?") {
-        s.pop();
-        v.push(s.clone() + "#");
-        v.push(s + ".");
-    } else {
-        v.push(s);
+fn combos(p: &str, n: &[usize], cache: &mut HashMap<(Box<str>, Box<[usize]>), i32>) -> i32 {
+    if p.len() == 0 {
+        return if n.len() == 0 { 1 } else { 0 };
     }
-    v
-}
-
-fn start_is_less_than2(s: &String, n: Vec<i32>, map: &mut HashMap<String, Vec<i32>>) -> bool {
-    // get up to the last char of s from the map to get the lengths
-    // add the last char of s to the lengths by looking at the last two chars of s ## adds one to last length and .# adds one to last length and adds a new length
-    // if all but last lengths are equal to corresponding from n and the last length is less than or equal to the last matching value from n
-    if s.len() == 0 {
-        return true;
+    if n.len() == 0 {
+        return if p.find('#').is_none() { 1 } else { 0 };
     }
-    if s.len() == 1 {
-        let lengths = match s.as_str() {
-            "#" => vec![1],
-            "." => vec![],
-            _ => panic!("Invalid string"),
-        };
-        map.insert(s.clone(), lengths.clone());
-        return lengths.len() == 0 || lengths[0] <= n[0];
+
+    let key = (p.into(), n.into());
+    if let Some(c) = cache.get(&key) {
+        return *c;
     }
-    let mut lengths = map.get(&s[0..s.len() - 1].to_string()).unwrap().clone();
-    let l = lengths.len();
-    let last_char = &s[s.len() - 1..s.len()];
-    let second_last_char = &s[s.len() - 2..s.len() - 1];
-    // dbg!(s, &lengths, &second_last_char, &last_char);
-    match (second_last_char, last_char) {
-        ("#", "#") => lengths[l - 1] += 1,
-        (".", "#") => lengths.push(1),
-        (_, ".") => (),
-        _ => panic!("Invalid string"),
-    };
-    map.insert(s.clone(), lengths.clone());
 
-    if lengths.len() == 0 {
-        return true;
-    } else if lengths.len() == 1 {
-        return lengths[0] <= n[0];
-    } else if lengths.len() > n.len() {
-        return false;
+    let mut count = 0;
+    if p.chars().next().unwrap() != '#' {
+        count += combos(&p[1..], &n, cache);
     }
-    lengths
-        .iter()
-        .take(lengths.len() - 1)
-        .zip(n.iter())
-        .all(|(l, n)| l == n)
-        && lengths.last().unwrap() <= &n[lengths.len() - 1]
-}
-
-fn combos(p: &String, n: Vec<i32>) -> i32 {
-    // take every possible combination of
-    let mut map = HashMap::<String, Vec<i32>>::new();
-    let mut stack = VecDeque::<String>::new();
-    variants(&p[0..1].to_string())
-        .iter()
-        .for_each(|v| stack.push_back(v.clone()));
-
-    let mut combos = 0;
-    while let Some(s) = stack.pop_front() {
-        if s.len() == p.len() {
-            if valid(&s, n.clone()) {
-                combos += 1;
-            }
-            continue;
-        } else if start_is_less_than2(&s, n.clone(), &mut map) {
-            // create variants of s with an additional char from p and push them to stack
-            let s_plus_one = s.clone() + &p[s.len()..s.len() + 1];
-            variants(&s_plus_one)
-                .iter()
-                .for_each(|v| stack.push_back(v.clone()));
+    if p.chars().next().unwrap() != '.' {
+        let p_chars: Vec<char> = p.chars().collect(); // Convert p to a vector of characters
+        if n[0] <= p_chars.len()
+            && p_chars[0..n[0]].iter().all(|&c| c != '#')
+            && (n[0] == p_chars.len() || p_chars[n[0]] != '#')
+        {
+            count += combos(&p[(n[0])..], &n[1..], cache);
         }
     }
-    combos
+    cache.insert(key, count);
+    count
 }
 
 fn main() {
+    let mut cache = HashMap::new();
     let file: String = std::env::args()
         .collect::<Vec<String>>()
         .get(1)
@@ -114,34 +50,24 @@ fn main() {
             (
                 p.to_string(),
                 n.split(",")
-                    .map(|s| s.parse::<i32>().unwrap())
+                    .map(|s| s.parse::<usize>().unwrap())
                     .collect_vec(),
             )
         });
 
-    // let p1_combos = parsed.clone().enumerate().map(|(i, (p, n))| {
-    //     dbg!(i);
-    //     combos(&p, n)
-    // });
-    // let p1 = p1_combos.sum::<i32>();
-    // println!("Part 1: {}", p1);
+    let p1_combos = parsed.clone().map(|(p, n)| combos(&p, &n, &mut cache));
+    let p1 = p1_combos.sum::<i32>();
+    println!("Part 1: {}", p1);
 
     let p2_combos = parsed
         .clone()
         .map(|(p, n)| {
-            // make p itself copied 5 times
-            // also do the same for n
-            // put a ? between each of the 5 copies of p
             (
                 (p.clone() + "?").repeat(4) + &p,
                 n.iter().cycle().take(5 * n.len()).cloned().collect_vec(),
             )
         })
-        .enumerate()
-        .map(|(i, (p, n))| {
-            dbg!(i);
-            combos(&p, n)
-        });
+        .map(|(p, n)| combos(&p, &n, &mut cache));
 
     let p2 = p2_combos.sum::<i32>();
     println!("Part 2: {}", p2);
