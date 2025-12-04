521943f5ff3c503880c67d839ec112fdefe62152
noahpro <noahpro@gmail.com>
2025-12-04 02:20:27 -0500

    day 4

--- 2025/day4/day4.py
+++ 2025/day4/day4.py
@@ -2,31 +2,31 @@
 f = open("input.txt").read().splitlines()
 X, Y = len(f[0]), len(f)
 
-grid = [[char for c, char in enumerate(row)] for r, row in enumerate(f)]
-dir = [
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
-]
-p1 = 0
-for r in range(X):
-    for c in range(Y):
-        pc = 0
-        if grid[r][c] == ".":
-            continue
-        for dr, dc in dir:
-            rr, cc = r + dr, c + dc
-            if rr < 0 or rr >= X or cc < 0 or cc >= Y:
+grid = [[char for char in row] for row in f]
+dir = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+p2 = 0
+while True:
+    removed = []
+    for r in range(X):
+        for c in range(Y):
+            pc = 0
+            if grid[r][c] == ".":
                 continue
-            if grid[rr][cc] == "@":
-                pc += 1
-        if pc < 4:
-            p1 += 1
+            for dr, dc in dir:
+                rr, cc = r + dr, c + dc
+                if rr < 0 or rr >= X or cc < 0 or cc >= Y:
+                    continue
+                if grid[rr][cc] == "@":
+                    pc += 1
+            if pc < 4:
+                removed.append((r, c))
 
-print(p1)
-# %%
+    for r, c in removed:
+        grid[r][c] = "."
+    if len(removed) == 0:
+        break
+    if p2 == 0:
+        print(len(removed))
+    p2 += len(removed)
+
+print(p2)
