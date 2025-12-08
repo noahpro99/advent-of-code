1a36bd2d2df83ade8e010aff13fa2b8d9a500a1e
noahpro <noahpro@gmail.com>
2025-12-08 13:25:28 -0500

    day 6

--- 2025/day6/day6.py
+++ 2025/day6/day6.py
@@ -1,23 +1,39 @@
 # %%
-
 import math
 
 import numpy as np
 
 f = open("input.txt").read().splitlines()
-grid = [[char for char in row.split()] for r, row in enumerate(f)]
+grid = [[char for char in row.split()] for row in f]
 ops = grid[-1]
 numbers = np.transpose(grid[:-1]).tolist()
 p1 = 0
 for nums, op in zip(numbers, ops):
     nums = list(map(int, nums))
-    print(nums, op)
     if op == "+":
         p1 += sum(nums)
     else:
         p1 += math.prod(nums)
 
-p1
+p2 = 0
+grid = [[char for char in row] for row in f]
+numbers = np.transpose(grid).tolist()
+acc = 0
+op = None
+for col in numbers:
+    if all(x == " " for x in col):
+        p2 += acc
+        acc = 0
+        continue
+    if col[-1] != " ":
+        op = col[-1]
+        if op == "*":
+            acc = 1
+    num = int("".join(col[:-1]))
+    if op == "*":
+        acc *= num
+    if op == "+":
+        acc += num
+p2 += acc
 
-
-# %%
+print(p1, p2)
