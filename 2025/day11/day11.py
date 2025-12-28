acdf8ea36ee6034d6873ebf2e28cde815774cdae
noahpro <noahpro@gmail.com>
2025-12-28 15:47:39 -0500

    day 11 smaller

--- 2025/day11/day11.py
+++ 2025/day11/day11.py
@@ -9,25 +9,23 @@ f = {
 
 @lru_cache
 def paths_through(cur: str) -> dict[str, int]:
-    ret: dict[str, int] = {"none": 0, "dac": 0, "fft": 0, "both": 0}
+    ret: dict[str, int] = {"out": 0, "dac": 0, "fft": 0, "both": 0}
     if cur == "out":
-        ret["none"] += 1
+        ret["out"] += 1
         return ret
     for child in f[cur]:
         p = paths_through(child)
         ret = {k: v + p[k] for k, v in ret.items()}
     if cur == "dac":
-        ret["dac"] += ret["none"]
-        ret["none"] = 0
+        ret["dac"] += ret["out"]
         ret["both"] += ret["fft"]
-        ret["fft"] = 0
     elif cur == "fft":
-        ret["fft"] += ret["none"]
-        ret["none"] = 0
+        ret["fft"] += ret["out"]
         ret["both"] += ret["dac"]
-        ret["dac"] = 0
     return ret
 
 
-print(sum(paths_through("you").values()))
+print(paths_through("you")["out"])
 print(paths_through("svr")["both"])
+
+# %%
